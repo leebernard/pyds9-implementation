a50f7c4b40753f3b70e5f38061b6be07936bdd12
leebernard <leebernard@users.noreply.github.com>
2018-08-20 14:57:21 -0700

    Began implementing a way of sigma clipped averaging of frames.
    It masks each data frame using sigma clipped stats, and adds the values
    to a holder array, with masked values set to zero. The number of values
    that have been added to a particular pixel are kept track of, by summing
    together the inverse of all the masks.

--- frame_average.py
+++ frame_average.py
@@ -6,7 +6,7 @@ import numpy as np
 import os
 
 from astropy.io import fits
-
+from astropy.stats import sigma_clip
 biasframe_path = '/home/lee/Documents/bias_frames'
 
 filename_list = os.listdir(biasframe_path)
@@ -14,5 +14,19 @@ filename_list = os.listdir(biasframe_path)
 # ensure all filenames have the proper extension
 fits_list = [biasframe_path + '/' + filename for filename in filename_list if filename[-8:] in ['.fits.fz']]
 
-# acquire the size of the image, number of tiles, etc
-hdul = fits.open(fits_list[0])
+# generate an array of zeros the size of the array
+average_image = np.zeros(fits.getdata(fits_list[0]).shape)
+# generate an array that keeps track of how many pixels have been added, on a pixel by pixel basis
+pixel_counter = np.copy(average_image)
+
+# iterate through the list of files, generating an average
+for fits_file in fits_list:
+    with fits.open(fits_file) as hdul:
+        # extract the sigma clipped data
+        print(hdul[1].data.shape)
+        clipped_data = sigma_clip(hdul[1].data, sigma=4)
+        # add the sigma clipped data to the image array average, with clipped values set to zero
+        average_image = average_image + clipped_data.filled(0)
+        # keep tract of which pixels had a value added to them
+        pixel_counter = pixel_counter + ~clipped_data.mask
+
--- frame_subtract.py
+++ frame_subtract.py
@@ -5,11 +5,13 @@ import numpy as np
 import pyds9
 from astropy.io import fits
 
-# filename1 = '/home/lee/Documents/bias_frames/c4d_170331_202028_zri.fits.fz'
-# filename2 = '/home/lee/Documents/bias_frames/c4d_170331_191113_zri.fits.fz'
+import timing
 
-filename1 = '/home/lee/Documents/k4m_160531_050920_ori.fits.fz'
-filename2 = '/home/lee/Documents/k4m_161228_132947_dri.fits.fz'
+filename1 = '/home/lee/Documents/bias_frames/c4d_170331_202028_zri.fits.fz'
+filename2 = '/home/lee/Documents/bias_frames/c4d_170331_191113_zri.fits.fz'
+
+# filename1 = '/home/lee/Documents/k4m_160531_050920_ori.fits.fz'
+# filename2 = '/home/lee/Documents/k4m_161228_132947_dri.fits.fz'
 
 # Open a new ds9 instance, or if already open, access it
 display = pyds9.DS9(target='display', start='-title display')
@@ -17,12 +19,15 @@ display = pyds9.DS9(target='display', start='-title display')
 with  fits.open(filename1) as minuend_hdul, fits.open(filename2) as subtrahend_hdul:
     # minuend_hdul.info()
     # subtrahend_hdul.info()
+
     # use a generator expression to conserve memory
     difference = (minuend.data.astype('float64', casting='safe') - subtrahend.data.astype('float64', casting='safe')
                   for minuend, subtrahend in zip(minuend_hdul, subtrahend_hdul)
                   if minuend.data is not None and subtrahend.data is not None)
 
+    data_list = []
     for data in difference:
-        display.set('frame new')
-        display.set_np2arr(data)
+        data_list.append(data)
+
+
 
