703c93239bf32197377f2b837336c8759d503dff
leebernard <leebernard@users.noreply.github.com>
2018-08-20 16:48:12 -0700

    Finished implementing a method of computing averages.
    It needs to be tested

--- frame_average.py
+++ frame_average.py
@@ -4,6 +4,7 @@ A function for pixel by pixel averaging of an arbitrary number of frames
 
 import numpy as np
 import os
+import pyds9
 
 from astropy.io import fits
 from astropy.stats import sigma_clip
@@ -19,14 +20,24 @@ average_image = np.zeros(fits.getdata(fits_list[0]).shape)
 # generate an array that keeps track of how many pixels have been added, on a pixel by pixel basis
 pixel_counter = np.copy(average_image)
 
-# iterate through the list of files, generating an average
+# iterate through the list of files, summing up all values of corresponding pixels
 for fits_file in fits_list:
     with fits.open(fits_file) as hdul:
         # extract the sigma clipped data
         print(hdul[1].data.shape)
-        clipped_data = sigma_clip(hdul[1].data, sigma=4)
+        clipped_data = sigma_clip(hdul[2].data, sigma=5)
         # add the sigma clipped data to the image array average, with clipped values set to zero
         average_image = average_image + clipped_data.filled(0)
         # keep tract of which pixels had a value added to them
         pixel_counter = pixel_counter + ~clipped_data.mask
 
+# set zero values to 1, to prevent dividing zero by zero
+pixel_counter[pixel_counter == 0] = 1
+
+# compute average
+return_value = average_image/pixel_counter
+
+ds9 = pyds9.DS9()
+
+print(np.histogram(pixel_counter, bins=np.arange(25)+.5))
+ds9.set_np2arr(return_value)
