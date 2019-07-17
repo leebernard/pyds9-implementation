73191486e78460f762c7df766a0f29e91fea2b2f
leebernard <ldbernar@ucsc.edu>
2019-07-17 15:27:45 -0700

    Add routine for calculating bias from the data files.

--- darkframe_analysis.py
+++ darkframe_analysis.py
@@ -13,6 +13,14 @@ from ccd_tools import *
 '''
 Starting out with -25 C
 '''
-bias_path = '/home/lee/Data/darkcurrent_25c'
+
+# retrieve everything from the bias directory, ignoring files that are not fits
+bias_path = '/home/lee/Data/darkcurrent_25c/bias_frames'
 bias_files = get_filenames(bias_path, extension='.fits')
+
+bias_mean, bias_median, stacked_bias_stddev = sigma_clipped_frame_stats(bias_files, path=bias_path, sigma=4.0)
+
+bias_25c = bias_mean[0]
+
+data_path = '/home/lee/Data/darkcurrent_25c/'
 
